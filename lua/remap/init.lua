4f5e182607643eb08ac03a2b8834c666bb8739be
Vinicius Andrade <vinicius.andrade00@gmail.com>
2024-04-09 01:50:40 -0300

    fixing module

--- lua/remap/init.lua
+++ lua/remap/init.lua
@@ -1,5 +1,8 @@
 local M = {}
-M.setup = function(opts)
-
+function M.setup(opts)
+    local start = vim.health.start or vim.health.report_start
+    local ok = vim.health.ok or vim.health.report_ok
+    ok('remap module is loaded')
+    start('remap module started')
 end
 return M
